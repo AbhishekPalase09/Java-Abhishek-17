916341fa946ecce18557468ef9d42b17e1a4d86b
AbhishekPalase09 <abhishekpalase09@gmail.com>
2026-01-18 22:27:57 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        char max = '0';
+        String s="";
+
+        if(num.charAt(num.length()-1) % 2 != 0)
+        return num;
+        for (int i = 0; i < num.length(); i++) {
+
+            if (num.charAt(i) % 2 != 0) {
+                    if (num.charAt(i) > max) {
+                        max = num.charAt(i);
+                    }
+            }
+          
+            s = String.valueOf(max);
+        }
+         
+        return s;
+    }
+}
