4135503334bc427af0d8d5e986dcf9c8cc073b80
AbhishekPalase09 <abhishekpalase09@gmail.com>
2026-02-23 23:41:27 +0530

    Add MathFunc class with basic math operations

---
+++ MathFunc.java
@@ -0,0 +1,11 @@
+public class MathFunc {
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-10));   // Absolute value
+        System.out.println(Math.sqrt(25));   // Square root
+        System.out.println(Math.pow(2, 3));  // Power (2^3)
+        System.out.println(Math.max(10, 20));// Maximum
+        System.out.println(Math.min(10, 20));// Minimum
+
+    }
+}
